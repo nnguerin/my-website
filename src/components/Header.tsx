203ca75dfe0017c3f34aac6da605eeae89e8b5a9
Nima Guerin <nnguerin@gmail.com>
2025-05-29 18:15:39 -0700

    added route

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,8 +4,9 @@ export default function Header() {
   return (
     <header className="p-2 flex gap-2 bg-white text-black justify-between">
       <nav className="flex flex-row">
-        <div className="px-2 font-bold">
+        <div className="px-2 font-bold flex gap-2">
           <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
         </div>
       </nav>
     </header>
